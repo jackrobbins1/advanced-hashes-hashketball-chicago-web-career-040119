80eb5fad219f324a7d266ff2c74703258b178995
Jack Robbins <jackrobbins1@gmail.com>
2019-03-26 20:55:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -286,6 +286,7 @@ def player_by_number(num)
   game_hash.each {|loc|
     loc[:players].each {|player|
       if player[:number] == num
+        puts player
         return player
       else
         next
